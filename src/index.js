4b29acf10a0a2c766a87bd176217b5a65dce39a8
giranezafiacre <51242074+giranezafiacre@users.noreply.github.com>
2020-08-18 20:38:43 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import 'dotenv/config';
 
 const server = express();
 
-// const port = process.env.PORT || 5000;
+const port = process.env.PORT || 5000;
 server.use(express.json());
 server.use(express.urlencoded({ extended: false }));
 server.get('/', (req, res) => res.status(200).json({ message: 'Welcome to my brand' }));
