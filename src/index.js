9d79759e521fef0ad63a426590eda3d8e7a50a2b
giranezafiacre <51242074+giranezafiacre@users.noreply.github.com>
2020-10-26 14:45:31 +0200

    Update index.js

--- src/index.js
+++ src/index.js
@@ -23,5 +23,18 @@ server.use(bodyParser.json());
 server.use(bodyParser.urlencoded({ extended: false }));
 server.get('/', (req, res) => res.status(200).json({ message: 'Welcome to my brand' }));
 server.use((req, res, next) => { res.status(400).json({ Error: 'Invalid Request' }); next();});
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  // res.setHeader('Origin', '*');
+  // Request methods you wish to allow
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  // Request headers you wish to allow
+  res.setHeader('Access-Control-Allow-Headers', 'Authorization ,content-type');
+  // Set to true if you need the website to include cookies in the requests sent
+  // to the API (e.g. in case you use sessions)
+  res.setHeader('Access-Control-Allow-Credentials', true);
+  // Pass to next layer of middleware
+  next();
+});
 server.listen(process.env.PORT || 5000, console.log(`server listening on ${port}`));
 export default server;
